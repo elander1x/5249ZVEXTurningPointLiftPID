89db136e19ab83c781ec2d83e64104d3841c13f1
JonPoret <44809358+JonPoret@users.noreply.github.com>
2018-11-17 21:00:52 -0500

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -1,10 +1,5 @@
 vex::brain Brain;
 vex::controller MainUser = vex::controller();
 vex::controller SecondUser = vex::controller(vex::controllerType::partner);
-
-vex::motor Claw = vex::motor(vex::PORT10,vex::gearSetting::ratio18_1,false);
-
-
 vex::motor RightLift = vex::motor(vex::PORT6,vex::gearSetting::ratio18_1,false);
 vex::motor LeftLift = vex::motor(vex::PORT5,vex::gearSetting::ratio18_1,true);
-
