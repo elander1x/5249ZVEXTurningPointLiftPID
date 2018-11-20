1210c56e15062a1bfe3ecd5209daae52493a8848
JonPoret <44809358+JonPoret@users.noreply.github.com>
2018-11-20 16:48:47 -0500

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -3,3 +3,7 @@ vex::controller MainUser = vex::controller();
 vex::controller SecondUser = vex::controller(vex::controllerType::partner);
 vex::motor RightLift = vex::motor(vex::PORT6,vex::gearSetting::ratio18_1,false);
 vex::motor LeftLift = vex::motor(vex::PORT5,vex::gearSetting::ratio18_1,true);
+vex::motor RightChassis = vex::motor(vex::PORT8,vex::gearSetting::ratio18_1,true);
+vex::motor LeftChassis = vex::motor(vex::PORT3,vex::gearSetting::ratio18_1,true);
+
+vex::gyro Gyroscope = vex::gyro(Brain.ThreeWirePort.B);
